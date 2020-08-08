28e7ca28c1085a16b44c0acd6364711a084536b3
Rebecca Congi <rncongi@gmail.com>
2020-08-08 12:22:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,8 +46,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |day, supplies|
-      
-      puts "  #{day.split("_").capitalize}: " + supplies.join(", ")
+      puts "  #{day.capitalize}: " + supplies.join(", ")
+      #puts "  #{day.split("_").capitalize}: " + supplies.join(", ")
     end
   end
 end
